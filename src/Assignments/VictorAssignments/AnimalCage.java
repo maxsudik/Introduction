57e221cb594237bc0d207dcbbcea19cf00a2d38d
maxsudik <maxsudik@gmail.com>
2020-03-31 14:54:33 -0700

    Ok, I'm lost with these changes.
    If everything before made sense for me, so now I'm not sure what is the point of this constructor
    
    This part is clear:
    '    //This is an instance variable of Animal type
         Animal animal1;
    '
    
    And this part looks for me as a bunch of animals doing I don't know what lol:
    '
    //This is constructor
        public AnimalCage(Animal animal1) {
            this.animal1 = animal1;
    '

--- src/Assignments/VictorAssignments/AnimalCage.java
+++ src/Assignments/VictorAssignments/AnimalCage.java
@@ -2,11 +2,14 @@ package Assignments.VictorAssignments;
 
 public class AnimalCage {
 
-    //This method is used to for animals to make noise
-    public void makeNoise() {
+    //This is an instance variable of Animal type
+    Animal animal1;
 
-        //This is an instance variable of Animal type
-        Animal animal1;
+    //This is constructor
+    public AnimalCage(Animal animal1) {
+        this.animal1 = animal1;
+
+
 
     }
 }
