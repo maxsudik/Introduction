e73f99925bb435a1ebc6a0196aab04a139887d15
Max Sudik <maxsudik@Maxs-MacBook-Pro.local>
2020-03-30 17:18:42 -0700

    Initial commit

---
+++ src/Assignments/VictorAssignments/AnimalCage.java
@@ -0,0 +1,8 @@
+package Assignments.VictorAssignments;
+
+public class AnimalCage {
+    public static void main(String[] args) {
+
+        AnimalCage animal1 = new AnimalCage();
+    }
+}
---
+++ src/Assignments/VictorAssignments/Zoo.java
@@ -0,0 +1,6 @@
+package Assignments.VictorAssignments;
+
+public class Zoo {
+    public static void main(String[] args) {
+    }
+}
